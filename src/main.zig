3e013e3c1fdbcee25084f96f5ceebea3b5a062ba
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-09-17 21:05:29 +0200

    Finish io

--- src/main.zig
+++ src/main.zig
@@ -15,24 +15,27 @@ pub fn main() !void {
 
     while (true) {
         const stdin = std.io.getStdIn();
+        const stdout = std.io.getStdOut();
+
         var buffer: [83]u8 = undefined;
         const n = try stdin.read(&buffer);
         if (n == 0) std.process.exit(0);
         if (n != 83) return Errors.InputError;
 
-        for (buffer[0..75], 0..) |c, i| {
+        for (buffer[0..81], 0..) |c, i| {
             if (c != '.') {
                 const j: u16 = @truncate(i);
-                print("PLACE: {} {} {}\n", .{ j % 9, j / 9, c - '1' });
                 sudoku.place(j / 9, j % 9, c - '1');
             }
         }
 
-        std.debug.print("Solving...\n", .{});
         sudoku.solve();
-        std.debug.print("Solved\n", .{});
+        for (sudoku.solution[0..]) |*c| {
+            c.* += '1';
+        }
+        _ = try stdout.write(sudoku.solution[0..]);
+        _ = try stdout.write(&.{'\n'});
 
-        print("Resetting\n", .{});
         var i: u16 = 81;
         while (true) {
             if (i == 0) break;
@@ -42,14 +45,5 @@ pub fn main() !void {
                 sudoku.unplace81(@truncate(i), c - '1');
             }
         }
-        print("Done resetting", .{});
-        break;
     }
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
