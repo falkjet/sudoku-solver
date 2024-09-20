024efff0a04d1e455a4dfef3e53b0c68fbc8a08f
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-09-20 21:08:00 +0200

    Track heights of columns

--- src/Sudoku.zig
+++ src/Sudoku.zig
@@ -5,11 +5,18 @@ const column_count = 9 * 9 * 4;
 const normal_node_count = 4 * 9 * 9 * 9;
 const total_node_count = normal_node_count + column_count + 1;
 
+col_sizes: [column_count]u8,
 nodes: [total_node_count]Node,
 solution: [81]u8,
 
 pub fn init() !Sudoku {
-    var self = Sudoku{ .nodes = std.mem.zeroes([total_node_count]Node), .solution = std.mem.zeroes([81]u8) };
+    var self = Sudoku{
+        .col_sizes = undefined,
+        .nodes = std.mem.zeroes([total_node_count]Node),
+        .solution = std.mem.zeroes([81]u8),
+    };
+
+    @memset(self.col_sizes[0..], 9);
 
     // Setup header row
     for (0..column_count + 1) |i| {
@@ -96,16 +103,17 @@ fn algorithmX(self: *Sudoku, path: []u16, depth: u8) ?[]u16 {
         path[depth] = node;
         if (self.algorithmX(path, depth + 1)) |r| return r;
     }
+
     return null;
 }
 
 fn shortest_column(self: *Sudoku) u16 {
     var smallest = self.right_of(0);
-    var height = self.column_height(smallest);
+    var height = self.col_sizes[smallest];
 
     var col = self.right_of(smallest);
     while (col != 0) : (col = self.right_of(col)) {
-        const h = self.column_height(col);
+        const h = self.col_sizes[col - 1];
         if (h < height) {
             height = h;
             smallest = col;
@@ -126,19 +134,21 @@ fn column_height(self: *const Sudoku, col: u16) u8 {
 }
 
 fn chooserow(self: *Sudoku, i: u16) void {
-    var j = self.right_of(i);
     self.removecol(i);
+
+    var j = self.right_of(i);
     while (j != i) : (j = self.right_of(j)) {
         self.removecol(j);
     }
 }
 
 fn unchooserow(self: *Sudoku, i: u16) void {
-    self.reinsertcol(i);
-    var j = self.right_of(i);
-    while (j != i) : (j = self.right_of(j)) {
+    var j = self.left_of(i);
+    while (j != i) : (j = self.left_of(j)) {
         self.reinsertcol(j);
     }
+
+    self.reinsertcol(i);
 }
 
 // Takes node as argument and uninserts the header and removes all
@@ -148,10 +158,8 @@ fn removecol(self: *Sudoku, i: u16) void {
     while (j != i) : (j = self.nodes[j].below) {
         if (self.isheader(j)) {
             const node = self.nodes[j];
-            const left = node.left;
-            const right = node.right;
-            self.nodes[left].right = right;
-            self.nodes[right].left = left;
+            self.nodes[node.left].right = node.right;
+            self.nodes[node.right].left = node.left;
             continue;
         }
         self.removerow(j);
@@ -174,23 +182,46 @@ fn reinsertcol(self: *Sudoku, i: u16) void {
 fn removerow(self: *Sudoku, i: u16) void {
     var j = self.right_of(i);
     while (j != i) : (j = self.right_of(j)) {
-        const node2 = self.nodes[j];
-        self.nodes[node2.above].below = node2.below;
-        self.nodes[node2.below].above = node2.above;
+        self.col_sizes[colof(j)] -= 1;
+
+        const node = self.nodes[j];
+        self.nodes[node.above].below = node.below;
+        self.nodes[node.below].above = node.above;
     }
 }
 
 fn reinsertrow(self: *Sudoku, i: u16) void {
-    const node = self.nodes[i];
-    self.nodes[node.above].below = i;
-    self.nodes[node.below].above = i;
-
-    var j = node.right;
+    var j = self.right_of(i);
     while (j != i) : (j = self.right_of(j)) {
-        const node2 = self.nodes[j];
-        self.nodes[node2.above].below = j;
-        self.nodes[node2.below].above = j;
+        self.col_sizes[colof(j)] += 1;
+
+        const node = self.nodes[j];
+        self.nodes[node.above].below = j;
+        self.nodes[node.below].above = j;
     }
+}
+
+pub fn colof(node: u16) u16 {
+    const n = node - (column_count + 1);
+    const i = n / 4;
+    const pos: u16 = i / 9; // [0, 81⟩
+    const value: u16 = i % 9;
+    const row = pos / 9; // [0, 9⟩
+    const col = pos % 9; // [0, 9⟩
+    const square = col / 3 + 3 * (row / 3);
+
+    const a = 0 * 81 + pos;
+    const b = 1 * 81 + 9 * row + value;
+    const c = 2 * 81 + 9 * col + value;
+    const d = 3 * 81 + 9 * square + value;
+
+    return switch (n % @as(u16, 4)) {
+        0 => a,
+        1 => b,
+        2 => c,
+        3 => d,
+        else => @panic("Impossible"),
+    };
 }
 
 /// connect a left and right node with each other
@@ -199,15 +230,11 @@ fn linkneighbors(self: *Sudoku, left: u16, right: u16) void {
     self.nodes[right].left = left;
 }
 
-fn vertical_reinsert(self: *Sudoku, i: u16) void {
-    self.nodes[self.nodes[i].above].below = i;
-    self.nodes[self.nodes[i].below].above = i;
-}
-
 fn vertical_insert_above(self: *Sudoku, new: u16, node: u16) void {
     self.nodes[new].below = node;
     self.nodes[new].above = self.nodes[node].above;
-    self.vertical_reinsert(new);
+    self.nodes[self.nodes[new].above].below = new;
+    self.nodes[self.nodes[new].below].above = new;
 }
 
 // Utils to read data structure
